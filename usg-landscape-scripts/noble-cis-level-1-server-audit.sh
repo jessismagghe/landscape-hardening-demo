024be76c68e53426c4330fe1727d9d00a4181312
Jessi Smagghe <jessica.smagghe@canonical.com>
2025-07-16 11:51:38 -0600

    Sample landscape scripts

---
+++ usg-landscape-scripts/noble-cis-level-1-server-audit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+TAILORING_FILE="$LANDSCAPE_ATTACHMENTS/noble-cis-level-1-server.xml"
+
+sudo usg audit --tailoring-file "$TAILORING_FILE"
---
+++ usg-landscape-scripts/noble-cis-level1-server-fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TAILORING_FILE="$LANDSCAPE_ATTACHMENTS/noble-cis-level-1-server.xml"
+
+sudo usg fix --tailoring-file "$TAILORING_FILE"
+
+sudo restart
