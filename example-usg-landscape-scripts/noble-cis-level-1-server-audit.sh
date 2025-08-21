4221997f27571961cf37a0ecfb9a1771dd5743f2
Jessi Smagghe <jessica.smagghe@canonical.com>
2025-08-21 10:34:12 -0600

    Add annotations

--- example-usg-landscape-scripts/noble-cis-level-1-server-audit.sh
+++ example-usg-landscape-scripts/noble-cis-level-1-server-audit.sh
@@ -3,3 +3,9 @@
 TAILORING_FILE="$LANDSCAPE_ATTACHMENTS/noble-cis-level-1-server.xml"
 
 sudo usg audit --tailoring-file "$TAILORING_FILE"
+
+KEY="cis_audit"
+VALUE="$(date +%Y-%m-%d)"
+ANNOTATION_DIR="/var/lib/landscape/client/annotations.d"
+
+echo "$VALUE" > "$ANNOTATION_DIR/$KEY"
